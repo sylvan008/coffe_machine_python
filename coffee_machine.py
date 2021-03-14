54e9f80adb3fdba04ff015d0e8a061dd182430de
sylvan008 <sylvan.climb@gmail.com>
2021-03-14 09:53:38 +0300

    calculation of the amount of ingredients

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,11 +1,20 @@
-# Write your code here
-message = """
-Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!
+def calculate_ingredients(cups_num=1):
+    water = 200 * cups_num
+    milk = 50 * cups_num
+    beans = 15 * cups_num
+    message = f"""
+For {cups_num} cups of coffee you will need:
+{water} ml of water
+{milk} ml of milk
+{beans} g of coffee beans
 """
-print(message.strip())
+    print(message.strip())
+
+
+def main():
+    print("Write how many cups of coffee you will need: ")
+    cups = int(input())
+    calculate_ingredients(cups)
+
+
+main()
