f3fe07baa4faf19e9ee8ae54d441fc6f1e0bfaab
sylvan008 <sylvan.climb@gmail.com>
2021-03-14 09:35:15 +0300

    simulation of the program\n\n Printmessage about process

---
+++ coffee_machine.py
@@ -0,0 +1,11 @@
+# Write your code here
+message = """
+Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!
+"""
+print(message.strip())
